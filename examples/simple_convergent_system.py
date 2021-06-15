1543120d4f13a2eb85f3321e27e3aa36842495c9
Renan Passos <renanpassos@poli.ufrj.br>
2021-06-15 20:10:32 -0300

    Converte indentação para tabs

--- examples/simple_convergent_system.py
+++ examples/simple_convergent_system.py
@@ -5,12 +5,12 @@ import matplotlib.pyplot as plt
 
 
 def system(x, t):
-    return (x[1], -x[0] - x[1])
+	return (x[1], -x[0] - x[1])
 
 
 arrows = [
-    np.linspace(-3, 3, 17),
-    np.linspace(-3, 3, 17)
+	np.linspace(-3, 3, 17),
+	np.linspace(-3, 3, 17)
 ]
 
 nlsystem.plot_phase_plane(system, (), arrows)
--- tests/test_plot_phase_plane.py
+++ tests/test_plot_phase_plane.py
@@ -6,12 +6,12 @@ import nlsystem
 
 
 def test_plot_phase_plane():
-    def system(x, t):
-        return (x[1], -x[0])
+	def system(x, t):
+		return (x[1], -x[0])
 
-    arrows = [
-        np.linspace(-3, 3, 17),
-        np.linspace(-3, 3, 17)
-    ]
+	arrows = [
+		np.linspace(-3, 3, 17),
+		np.linspace(-3, 3, 17)
+	]
 
-    nlsystem.plot_phase_plane(system, (), arrows)
+	nlsystem.plot_phase_plane(system, (), arrows)
